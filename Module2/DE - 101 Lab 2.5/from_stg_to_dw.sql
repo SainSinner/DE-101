0807479cdbb474def3aa5111bd8bc670b9b73103
SainSinner <g.cvyat@gmail.com>
2024-01-07 02:39:47 +0500

    1

--- Module2/DE - 101 Lab 2.5/from_stg_to_dw.sql
+++ Module2/DE - 101 Lab 2.5/from_stg_to_dw.sql
@@ -1,8 +1,8 @@
--- create schema
--- create dim tables (shipping, customer, product, geo)
--- fix data quality problem
--- create sales_fact table
--- match number of rows between staging and dw (business layer)
+--  create schema
+--  create dim tables (shipping, customer, product, geo)
+--  fix data quality problem
+--  create sales_fact table
+--  match number of rows between staging and dw (business layer)
 
 
 
@@ -13,9 +13,9 @@ create schema dw;
 
 
 
---SHIPPING
+-- SHIPPING
 
---creating a table
+-- creating a table
 drop table if exists dw.shipping_dim ;
 CREATE TABLE dw.shipping_dim
 (
@@ -24,41 +24,41 @@ CREATE TABLE dw.shipping_dim
  CONSTRAINT PK_shipping_dim PRIMARY KEY ( ship_id )
 );
 
---deleting rows
+-- deleting rows
 truncate table dw.shipping_dim;
 
---generating ship_id and inserting ship_mode from orders
+-- generating ship_id and inserting ship_mode from orders
 insert into dw.shipping_dim 
 select 100+row_number() over(), ship_mode from (select distinct ship_mode from stg.orders ) a;
---checking
+-- checking
 select * from dw.shipping_dim sd; 
 
 
 
 
---CUSTOMER
+-- CUSTOMER
 
 drop table if exists dw.customer_dim ;
 CREATE TABLE dw.customer_dim
 (
 cust_id serial NOT NULL,
-customer_id   varchar(8) NOT NULL, --id can't be NULL
+customer_id   varchar(8) NOT NULL,--  id can't be NULL
  customer_name varchar(22) NOT NULL,
  CONSTRAINT PK_customer_dim PRIMARY KEY ( cust_id )
 );
 
---deleting rows
+-- deleting rows
 truncate table dw.customer_dim;
---inserting
+-- inserting
 insert into dw.customer_dim 
 select 100+row_number() over(), customer_id, customer_name from (select distinct customer_id, customer_name from stg.orders ) a;
---checking
+-- checking
 select * from dw.customer_dim cd;  
 
 
 
 
---GEOGRAPHY
+-- GEOGRAPHY
 
 drop table if exists dw.geo_dim ;
 CREATE TABLE dw.geo_dim
@@ -67,25 +67,25 @@ CREATE TABLE dw.geo_dim
  country     varchar(13) NOT NULL,
  city        varchar(17) NOT NULL,
  state       varchar(20) NOT NULL,
- postal_code varchar(20) NULL,       --can't be integer, we lost first 0
+ postal_code varchar(20) NULL,       --  can't be integer, we lost first 0
  CONSTRAINT PK_geo_dim PRIMARY KEY ( geo_id )
 );
 
---deleting rows
+-- deleting rows
 truncate table dw.geo_dim;
---generating geo_id and inserting rows from orders
+-- generating geo_id and inserting rows from orders
 insert into dw.geo_dim 
 select 100+row_number() over(), country, city, state, postal_code from (select distinct country, city, state, postal_code from stg.orders ) a;
---data quality check
+-- data quality check
 select distinct country, city, state, postal_code from dw.geo_dim
 where country is null or city is null or postal_code is null;
 
--- City Burlington, Vermont doesn't have postal code
+--  City Burlington, Vermont doesn't have postal code
 update dw.geo_dim
 set postal_code = '05401'
 where city = 'Burlington'  and postal_code is null;
 
---also update source file
+-- also update source file
 update stg.orders
 set postal_code = '05401'
 where city = 'Burlington'  and postal_code is null;
@@ -97,14 +97,14 @@ where city = 'Burlington'
 
 
 
---PRODUCT
+-- PRODUCT
 
---creating a table
+-- creating a table
 drop table if exists dw.product_dim ;
 CREATE TABLE dw.product_dim
 (
- prod_id   serial NOT NULL, --we created surrogated key
- product_id   varchar(50) NOT NULL,  --exist in ORDERS table
+ prod_id   serial NOT NULL, -- we created surrogated key
+ product_id   varchar(50) NOT NULL,  -- exist in ORDERS table
  product_name varchar(127) NOT NULL,
  category     varchar(15) NOT NULL,
  sub_category varchar(11) NOT NULL,
@@ -112,20 +112,20 @@ CREATE TABLE dw.product_dim
  CONSTRAINT PK_product_dim PRIMARY KEY ( prod_id )
 );
 
---deleting rows
+-- deleting rows
 truncate table dw.product_dim ;
---
+-- 
 insert into dw.product_dim 
 select 100+row_number() over () as prod_id ,product_id, product_name, category, subcategory, segment from (select distinct product_id, product_name, category, subcategory, segment from stg.orders ) a;
---checking
+-- checking
 select * from dw.product_dim cd; 
 
 
 
---CALENDAR use function instead 
--- examplehttps://tapoueh.org/blog/2017/06/postgresql-and-the-calendar/
+-- CALENDAR use function instead 
+--  examplehttps://tapoueh.org/blog/2017/06/postgresql-and-the-calendar/
 
---creating a table
+-- creating a table
 drop table if exists dw.calendar_dim ;
 CREATE TABLE dw.calendar_dim
 (
@@ -140,9 +140,9 @@ leap  varchar(20) NOT NULL,
 CONSTRAINT PK_calendar_dim PRIMARY KEY ( dateid )
 );
 
---deleting rows
+-- deleting rows
 truncate table dw.calendar_dim;
---
+-- 
 insert into dw.calendar_dim 
 select 
 to_char(date,'yyyymmdd')::int as date_id,  
@@ -160,16 +160,16 @@ to_char(date,'yyyymmdd')::int as date_id,
                        date '2030-01-01',
                        interval '1 day')
        as t(date);
---checking
+-- checking
 select * from dw.calendar_dim; 
 
 
 
 
 
---METRICS
+-- METRICS
 
---creating a table
+-- creating a table
 drop table if exists dw.sales_fact ;
 CREATE TABLE dw.sales_fact
 (
@@ -204,12 +204,12 @@ select
 	 ,discount
 from stg.orders o 
 inner join dw.shipping_dim s on o.ship_mode = s.shipping_mode
-inner join dw.geo_dim g on o.postal_code = g.postal_code and g.country=o.country and g.city = o.city and o.state = g.state --City Burlington doesn't have postal code
+inner join dw.geo_dim g on o.postal_code = g.postal_code and g.country=o.country and g.city = o.city and o.state = g.state -- City Burlington doesn't have postal code
 inner join dw.product_dim p on o.product_name = p.product_name and o.segment=p.segment and o.subcategory=p.sub_category and o.category=p.category and o.product_id=p.product_id 
 inner join dw.customer_dim cd on cd.customer_id=o.customer_id and cd.customer_name=o.customer_name 
 
 
---do you get 9994rows?
+-- do you get 9994rows?
 select count(*) from dw.sales_fact sf
 inner join dw.shipping_dim s on sf.ship_id=s.ship_id
 inner join dw.geo_dim g on sf.geo_id=g.geo_id
